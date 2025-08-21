9bbc185aa6af0f22e02f81409b442184fdb3436a
Ramyakomali08 <chittelaramya.2007@gmail.com>
2025-08-21 10:51:58 +0530

    Added new files

---
+++ Example5.js
@@ -0,0 +1,27 @@
+// import { createInterface } from "readline";
+// const rl = createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+// rl.question("Enter a number:",function(answer){
+//     const number = parseInt(answer);
+//     if(number>0){
+//         console.log("The number is positive");
+//     }else if(number==0){
+//         console.log("The number is zero");
+//     }else{
+//         console.log("The numberis negative");
+//     }
+//     rl.close();
+// });
+
+const number = parseInt(prompt("Enter a number: "));
+if (number > 0) {
+    console.log("The number is positive");
+}
+else if (number == 0) {
+  console.log("The number is zero");
+}
+else {
+     console.log("The number is negative");
+}
